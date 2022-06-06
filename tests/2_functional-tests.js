53e2feeb7c01ec28b0e143f551b1a497b660d46e
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2022-06-06 09:23:52 +0100

    complete first test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,30 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite("Functional Tests", function () {
+  this.timeout(5000);
+  suite("Integration tests with chai-http", function () {
+    // #1
+    test("Create an issue with every field: POST request to /api/issues/apitest", function (done) {
+      chai
+        .request(server)
+        .post("/api/issues/apitest")
+        .send({
+          issue_title: "Fix error in posting data",
+          issue_text: "When we post data it has an error.",
+          created_by: "Joe",
+          assigned_to: "Joe",
+          open: true,
+          status_text: "In QA",
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          done();
+        });
+    });
+  });
 });
